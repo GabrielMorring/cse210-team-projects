68f044450e1c070caa9866a9bd1ec0c86d443218
Gabriel Morring <gmorring7@gmail.com>
2022-02-10 08:12:22 -0800

    added jumper class

--- unit03-jumper/Game/Jumper.cs
+++ unit03-jumper/Game/Jumper.cs
@@ -3,5 +3,37 @@ using System.Collections.Generic;
 
 namespace unit03_jumper.Game
 {
-    
+    public class Jumper
+    {
+
+        int parachuteLives;
+
+        public Jumper()
+        {
+            parachuteLives = 4;
+        }
+
+        public bool CheckAlive()
+        {
+            if (parachuteLives == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        public void UpdateLives()
+        {
+            parachuteLives -= 1;
+        }
+
+        public int GetParachuteLives()
+        {
+            return parachuteLives;
+        }
+        
+    }
 }
