124f316f802e8d86d4a4611071097a6bc338e260
Gabriel Morring <gmorring7@gmail.com>
2022-02-18 14:51:26 -0800

    ScoreBoard class updated

--- unit04-greed/Game/Casting/ScoreBoard.cs
+++ unit04-greed/Game/Casting/ScoreBoard.cs
@@ -6,6 +6,17 @@ namespace Unit04.Game.Casting
     
     public class ScoreBoard : Actor
     {
-        
+
+        public ScoreBoard()
+        {
+            SetText("Score:");
+            SetFontSize(25);
+            SetPosition(new Point(0,5));
+        }
+
+        public void UpdateText(int score)
+        {
+            SetText("$Score: {score}");
+        }
     }
 }
