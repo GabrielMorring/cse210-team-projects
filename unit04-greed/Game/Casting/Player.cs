00f7cd2a7a89b6459ea0c41618fdf54ef8dc7c8c
Gabriel Morring <gmorring7@gmail.com>
2022-02-19 15:25:16 -0800

    did Player class

--- unit04-greed/Game/Casting/Player.cs
+++ unit04-greed/Game/Casting/Player.cs
@@ -6,6 +6,27 @@ namespace Unit04.Game.Casting
     
     public class Player : Actor
     {
-        
+
+      public Player(int max_x, int max_y)
+      {
+          SetText("#");
+          SetPosition(new Point(max_x/2, max_y))
+
+
+      }
+
+      public void MovePlayerNext(int maxX, int maxY)
+      {
+          if (GetVelocity().GetY() != 0)
+          {
+              
+              SetVelocity(new Point(GetVelocity().GetX(), 0));
+              MoveNext(maxX, maxY);
+          }
+          else
+          {
+              MoveNext(maxX,maxY);
+          }
+      }  
     }
 }
