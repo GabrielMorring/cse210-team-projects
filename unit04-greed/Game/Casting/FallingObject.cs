6e1595c974c8faed59b9ec78b3c857147652c48c
daviepineda1 <87446052+daviepineda1@users.noreply.github.com>
2022-02-21 15:41:56 -0700

    Set FallingObject Position and Velocity

--- unit04-greed/Game/Casting/FallingObject.cs
+++ unit04-greed/Game/Casting/FallingObject.cs
@@ -3,13 +3,31 @@ using System;
 
 namespace Unit04.Game.Casting
 {
+
+    /// <summary>
+    /// 
+    ///
+    /// The responsibility of an FallingObject is to destroy object it hits.
+    /// 
+    /// </summary>
     
     public class FallingObject : Actor
     {
+        private Point _velocity = new Point(0,0);
 
        public FallingObject()
        {
+         SetPosition(new Point(3,2));
+
+         SetVelocity(new Point(3,2));
          
        } 
-    }
+
+       
+
+      
+      
+
+
+       
 }
