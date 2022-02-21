d6c50978ca02459bc1de042a1757523b20ec3438
daviepineda1 <87446052+daviepineda1@users.noreply.github.com>
2022-02-21 15:56:05 -0700

    FallingObject Syntax Error

--- unit04-greed/Game/Casting/FallingObject.cs
+++ unit04-greed/Game/Casting/FallingObject.cs
@@ -3,31 +3,17 @@ using System;
 
 namespace Unit04.Game.Casting
 {
-
-    /// <summary>
-    /// 
-    ///
-    /// The responsibility of an FallingObject is to destroy object it hits.
-    /// 
-    /// </summary>
+  
+  
+  public class FallingObject : Actor
+  {
+    private Point _velocity = new Point(0,0);
     
-    public class FallingObject : Actor
+    public FallingObject()
     {
-        private Point _velocity = new Point(0,0);
-
-       public FallingObject()
-       {
-         SetPosition(new Point(3,2));
-
-         SetVelocity(new Point(3,2));
-         
-       } 
-
+      SetPosition(new Point(3,2));
+      SetVelocity(new Point(3,2));
        
-
-      
-      
-
-
-       
+    }
+  }
 }
