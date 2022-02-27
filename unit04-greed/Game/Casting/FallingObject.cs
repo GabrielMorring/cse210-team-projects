9eb666113cd69cd7e9a526eaf652cab6fe258ce9
Carson Bradford <bra19028@byui.edu>
2022-02-26 20:58:32 -0700

    Comments added to FallingObject class

--- unit04-greed/Game/Casting/FallingObject.cs
+++ unit04-greed/Game/Casting/FallingObject.cs
@@ -3,12 +3,21 @@ using System;
 
 namespace Unit04.Game.Casting
 {
-  
+  /// <summary>
+    /// <para>A thing that participates in the game.</para>
+    /// <para>
+    /// The responsibility of Falling Object is to keep track of its appearance, position and velocity in 2d 
+    /// space as it falls down the screen.
+    /// </para>
+    /// </summary>
   
   public class FallingObject : Actor
   {
     private Point _velocity = new Point(0,0);
     
+    /// <summary>
+    /// Constructs a new instance of FallingObject.
+    /// </summary>
     public FallingObject()
     {
       SetPosition(new Point(0,2));
@@ -16,7 +25,10 @@ namespace Unit04.Game.Casting
        
     }
 
-
+    /// <summary>
+    /// Updates the position of the falling object, causing the object to 'fall' down the screen.
+    /// </summary>
+    /// <returns> void </returns>
     public void DoUpdates()
     {
       // 
