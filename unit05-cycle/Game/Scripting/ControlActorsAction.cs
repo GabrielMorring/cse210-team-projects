71e316d8b2a8ec4a7991483ae021eac1e45eb6f5
Carson Bradford <bra19028@byui.edu>
2022-03-03 22:51:11 -0700

    Two player controlls work properly

--- unit05-cycle/Game/Scripting/ControlActorsAction.cs
+++ unit05-cycle/Game/Scripting/ControlActorsAction.cs
@@ -13,7 +13,8 @@ namespace Unit05.Game.Scripting
     public class ControlActorsAction : Action
     {
         private KeyboardService keyboardService;
-        private Point direction = new Point(Constants.CELL_SIZE, 0);
+        private Point p1Direction = new Point(Constants.CELL_SIZE, 0);
+        private Point p2Direction = new Point(Constants.CELL_SIZE, 0);
 
         /// <summary>
         /// Constructs a new instance of ControlActorsAction using the given KeyboardService.
@@ -29,56 +30,56 @@ namespace Unit05.Game.Scripting
             // left
             if (keyboardService.IsKeyDown("a"))
             {
-                direction = new Point(-Constants.CELL_SIZE, 0);
+                p1Direction = new Point(-Constants.CELL_SIZE, 0);
             }
 
             // right
             if (keyboardService.IsKeyDown("d"))
             {
-                direction = new Point(Constants.CELL_SIZE, 0);
+                p1Direction = new Point(Constants.CELL_SIZE, 0);
             }
 
             // up
             if (keyboardService.IsKeyDown("w"))
             {
-                direction = new Point(0, -Constants.CELL_SIZE);
+                p1Direction = new Point(0, -Constants.CELL_SIZE);
             }
 
             // down
             if (keyboardService.IsKeyDown("s"))
             {
-                direction = new Point(0, Constants.CELL_SIZE);
+                p1Direction = new Point(0, Constants.CELL_SIZE);
             }
 
             Cycler player1 = (Cycler)cast.GetFirstActor("cycler");
-            player1.TurnHead(direction);
+            player1.TurnHead(p1Direction);
 
             // left
             if (keyboardService.IsKeyDown("j"))
             {
-                direction = new Point(-Constants.CELL_SIZE, 0);
+                p2Direction = new Point(-Constants.CELL_SIZE, 0);
             }
 
             // right
             if (keyboardService.IsKeyDown("l"))
             {
-                direction = new Point(Constants.CELL_SIZE, 0);
+                p2Direction = new Point(Constants.CELL_SIZE, 0);
             }
 
             // up
             if (keyboardService.IsKeyDown("i"))
             {
-                direction = new Point(0, -Constants.CELL_SIZE);
+                p2Direction = new Point(0, -Constants.CELL_SIZE);
             }
 
             // down
             if (keyboardService.IsKeyDown("k"))
             {
-                direction = new Point(0, Constants.CELL_SIZE);
+                p2Direction = new Point(0, Constants.CELL_SIZE);
             }
             
             Cycler player2 = (Cycler)cast.GetLastActor("cycler");
-            player2.TurnHead(direction);
+            player2.TurnHead(p2Direction);
         }
     }
 }
